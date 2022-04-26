29b58fa40a91efe1df25eae591130249ad8ac66d
Noah Cyrus <42313462+danny0012@users.noreply.github.com>
2022-04-26 11:38:31 +0400

    Add files via upload

---
+++ floats.go
@@ -0,0 +1,36 @@
+﻿// Package datafile reads data from files
+package datafile
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// GetFloats reads float64 value from strings of file
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return numbers, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		numberString := strings.TrimSpace(scanner.Text())
+		number, err := strconv.ParseFloat(numberString, 64)
+		if err != nil {
+			return numbers, err
+		}
+		numbers = append(numbers, number)
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+	
+	return numbers, nil
+}
